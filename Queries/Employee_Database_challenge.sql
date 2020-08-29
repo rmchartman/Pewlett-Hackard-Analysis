b4278e6315257c464149b0a44ccc7d0952a7f284
Renata Hartman <renata.mc.hartman@gmail.com>
2020-08-28 22:47:28 -0700

    Deliverable 2

--- Queries/Employee_Database_challenge.sql
+++ Queries/Employee_Database_challenge.sql
@@ -1,3 +1,4 @@
+-- Retiree titles
 SELECT e.emp_no, 
 e.first_name,
 e.last_name,
@@ -26,3 +27,19 @@ INTO retiring_titles
 FROM unique_titles ut
 GROUP BY ut.title
 ORDER BY COUNT(ut.title) DESC;
+
+-- Employees available for mentorship
+SELECT DISTINCT ON (e.emp_no) e.emp_no, 
+e.first_name,
+e.last_name,
+e.birth_date,
+de.from_date,
+de.to_date,
+t.title
+INTO mentorship_eligibilty
+FROM employees e
+LEFT JOIN dept_emp de on e.emp_no = de.emp_no
+LEFT JOIN titles t ON e.emp_no = t.emp_no
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND de.to_date = ('9999-01-01')
+ORDER BY e.emp_no, t.to_date
