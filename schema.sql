56766e05436834431043ad96ec5304701ae9dfa4
Renata Hartman <renata.mc.hartman@gmail.com>
2020-08-28 22:10:33 -0700

    completed module

--- schema.sql
+++ schema.sql
@@ -47,7 +47,7 @@ CREATE TABLE salaries (
 
 CREATE TABLE titles (
   emp_no INT NOT NULL,
-  title INT NOT NULL,
+  title VARCHAR(250) NOT NULL,
   from_date DATE NOT NULL,
   to_date DATE NOT NULL,
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
